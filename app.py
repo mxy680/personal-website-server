7fe03de9b6e6eaaba4c600afb9c9cd6b5d4c6e27
Mark <mark@mxy680.net>
2024-03-11 11:31:11 -0400

    using streamlit

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, jsonify, render_template, request
-import transformers
+import streamlit as st
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
-app = Flask(__name__)
-
-sentiment_model = transformers.pipeline('sentiment-analysis')
-
-
-@app.route('/sentiment-analysis', methods=['POST'])
-def predict():
-    message = request.get_json()['message']
-    results = sentiment_model(message)[0]
-    prediction = results["label"]
-    score = results["score"]
-    return jsonify({ 'prediction': prediction, 'score': score })
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = AutoTokenizer.from_pretrained("LiyaT3/sentiment-analysis-imdb-distilbert")
+    model = AutoModelForSequenceClassification.from_pretrained("LiyaT3/sentiment-analysis-imdb-distilbert")
+    return tokenizer, model
 
 
-if __name__ == '__main__':
-    # starting app on localhost:8080
-    app.run(port=8080)
+tokenizer, model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
+
+d = {
+    1:'POSITIVE',
+    0:'NEGATIVE'
+}
+
+if user_input and button:
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
