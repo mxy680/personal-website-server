09eaa66c61aa5ae3ac7c778123d52acf331cd596
Mark <mark@mxy680.net>
2024-03-11 11:32:49 -0400

    removed pycache

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ d = {
 
 if user_input and button:
     test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
-    # test_sample
     output = model(**test_sample)
     st.write("Logits: ",output.logits)
     y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
